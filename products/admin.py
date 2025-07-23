9a3988bc57f57100736a212e7702146891ae93dd
Danchik10 <164547411+Danchik10@users.noreply.github.com>
2025-07-23 23:59:02 +0300

    товары для бд и категории

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductsCategory, Product
+
+admin.site.register(ProductsCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from products.models import Product, ProductsCategory
 
-# Create your views here.
+def index(request):
+    context = {'title' : 'Store'}
+
+    return render(request, 'products/index.html', context)
+
+def products(request, category_id = None):
+    if category_id:
+        category = ProductsCategory.objects.get(id = category_id)
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+
+    context = {
+        'title' : 'Store - Каталог',
+        'categories' : ProductsCategory.objects.all(),
+        'products' : products
+    }
+    return render(request, 'products/products.html', context)
